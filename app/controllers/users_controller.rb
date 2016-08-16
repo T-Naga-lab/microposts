4089e0217a8e51db514fcef5718d4aa98b8ea3eb
Tomoya Nagaoka <tomoyanagaoka@yahoo.co.jp>
2016-08-16 07:09:46 +0000

    lesson7-1.rev06

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :region, :birth, :comment, :password, :password_confirmation)
   end
   
-  def correct_user
+  def current_user
     @user = User.find(params[:id])
     unless @user == current_user
     redirect_to(root_url)
